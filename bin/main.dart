9f32179bf0590f454fded6cc878fe09bb3b44482
zhangjiayu <kertlk@qq.com>
2023-06-08 14:52:22 +0800

    release: 0.1.2-M7

--- bin/main.dart
+++ bin/main.dart
@@ -7,7 +7,7 @@ import 'package:bili_novel_packer/pack_callback.dart';
 import 'package:console/console.dart';
 
 const String gitUrl = "https://gitee.com/Montaro2017/bili_novel_packer";
-const String version = "0.1.2-M6";
+const String version = "0.1.2-M7";
 
 void main(List<String> args) async {
   printWelcome();
