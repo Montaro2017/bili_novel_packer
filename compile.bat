211874753c22d7d803bef40ad53a79550e0dac00
zhangjiayu <kertlk@qq.com>
2023-12-26 10:14:38 +0800

    release: 0.2.8

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@
 del build /q /s /f
 rd build /q /s
 mkdir build
-dart compile exe bin/main.dart -o ./build/bili_novel_packer-0.2.7.exe
+dart compile exe bin/main.dart -o ./build/bili_novel_packer-0.2.8.exe
