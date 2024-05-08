4168b9de1581f7900c302a2b16625f2a6d64ed0e
zhangjiayu <kertlk@qq.com>
2024-05-08 11:44:46 +0800

    release: v0.2.15

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ import 'package:bili_novel_packer/pack_argument.dart';
 import 'package:console/console.dart';
 
 const String gitUrl = "https://gitee.com/Montaro2017/bili_novel_packer";
-const String version = "0.2.14";
+const String version = "0.2.15";
 
 void main(List<String> args) async {
   printWelcome();
