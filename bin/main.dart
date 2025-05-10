552ead4e902d5520158d1a39aad69742d45f2567
Spark <kertlk@qq.com>
2025-05-10 13:29:40 +0800

    release: v0.2.31

--- bin/main.dart
+++ bin/main.dart
@@ -7,7 +7,7 @@ import 'package:bili_novel_packer/pack_argument.dart';
 import 'package:console/console.dart';
 
 const String gitUrl = "https://github.com/Montaro2017/bili_novel_packer";
-const String version = "0.2.30";
+const String version = "0.2.31";
 
 void main(List<String> args) async {
   printWelcome();
