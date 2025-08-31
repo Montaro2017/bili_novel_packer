488b6d9cb7c99ad8a72a894e0480fbcecc8f8fe4
Dolphin-QvQ <DPHghost@126.com>
2025-08-31 11:55:55 +0800

    feat: chapterlog v1006b7

--- lib/light_novel/bili_novel/bili_novel_source.dart
+++ lib/light_novel/bili_novel/bili_novel_source.dart
@@ -302,7 +302,7 @@ class BiliNovelSource implements LightNovelSource {
         RegExp("chapterid:'(\\d+)'").firstMatch(doc.outerHtml)?.group(1) ?? '',
       );
       String jsSrc = script.attributes["src"]!;
-      String currentVersion = "v1006b6";
+      String currentVersion = "v1006b7";
       String matchedVersion = jsSrc.substring(jsSrc.lastIndexOf("v"));
       if (currentVersion != matchedVersion && !warnFlag) {
         print(
@@ -315,7 +315,7 @@ class BiliNovelSource implements LightNovelSource {
       }
       return {
         "fixedLength": 20,
-        "seed": chapterId * 135 + 234,
+        "seed": chapterId * 132 + 234,
         "a": 9302,
         "c": 49397,
         "mod": 233280
