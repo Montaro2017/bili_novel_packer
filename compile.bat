d66ddc06151bf5846e0a5baf3edfc2beef9cdda0
Dolphin-QvQ <DPHghost@126.com>
2025-08-31 11:57:58 +0800

    feat: chapterlog v1006b7
    未多本校验，请多样本校验后再进行合并
    当前test通过

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@
 del build /q /s /f
 rd build /q /s
 mkdir build
-dart compile exe bin/main.dart -o ./build/bili_novel_packer-0.2.36.exe
+dart compile exe bin/main.dart -o ./build/bili_novel_packer-0.2.37-test-v1006b7.exe
