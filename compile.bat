0ef7b58e9977d13a064c167577159b3ddce2f011
Spark <kertlk@qq.com>
2025-12-04 10:48:37 +0800

    release: v0.2.40

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@
 del build /q /s /f
 rd build /q /s
 mkdir build
-dart compile exe bin/main.dart -o ./build/bili_novel_packer0.2.39.exe
+dart compile exe bin/main.dart -o ./build/bili_novel_packer0.2.40.exe
